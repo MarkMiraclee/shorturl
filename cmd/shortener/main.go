b95cce30221697234981b87d3e103af6606e2495
MarkMiraclee <kozhevnikovv18@gmail.com>
2025-04-09 21:48:35 +0300

    Внёс правки по ревью

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"log"
 	"net/http"
 	"shorturl/internal/config"
 	"shorturl/internal/handlers"
-	"shorturl/internal/router"
+	"shorturl/internal/service"
 )
 
 func main() {
-	cfg := config.Load()
-	store := &handlers.URLStore{URLs: make(map[string]string)}
-	r := router.NewRouter(store, cfg)
-	http.ListenAndServe(cfg.ServerAddress, r)
+	cfg := config.Load() // Загрузка конфигурации приложения.
+
+	svc := service.NewURLService() // Создание экземпляра сервиса для работы с URL.
+	h := handlers.NewHandlers(svc) // Создание экземпляра обработчиков HTTP-запросов, передавая ему сервис.
+
+	r := chi.NewRouter()               // Создание нового роутера chi.
+	r.Post("/", h.HandlePost(cfg))     // Определение маршрута для POST-запросов на корневой путь.
+	r.Get("/{shortID}", h.HandleGet()) // Определение маршрута для GET-запросов с параметром shortID.
+
+	fmt.Printf("Server address from config: %s\n", cfg.ServerAddress)
+	fmt.Printf("Starting server on %s\n", cfg.BaseURL)
+	log.Fatal(http.ListenAndServe(cfg.ServerAddress, r))
 }
